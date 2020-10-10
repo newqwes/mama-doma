2e2c02565a2e188d1ff405290c8de99e845f3e6f
Nazar Ashirov <newnewqwes@gmail.com>
2020-10-10 22:51:15 +0300

    size

--- js/popup.js
+++ js/popup.js
@@ -96,20 +96,20 @@ document.addEventListener('keydown', function (e) {
             popupClose(popupActive)
         }
     })
-    (function () {
-        if (!Element.prototype.closest) {
-            Element.prototype.closest = function (css) {
-                var node = this;
-                while (node) {
-                    if (node.matches(css)) return node;
-                    else node = node.parentElement;
-                }
-                return null;
-            }
-        }
-    })();
-(function () {
-    if (!Element.prototype.matches) {
-        Element.prototype.matches = Element.prototype.matchesSelector || Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector;
-    }
-})();
+    // (function () {
+    //     if (!Element.prototype.closest) {
+    //         Element.prototype.closest = function (css) {
+    //             var node = this;
+    //             while (node) {
+    //                 if (node.matches(css)) return node;
+    //                 else node = node.parentElement;
+    //             }
+    //             return null;
+    //         }
+    //     }
+    // })();
+// (function () {
+//     if (!Element.prototype.matches) {
+//         Element.prototype.matches = Element.prototype.matchesSelector || Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector;
+//     }
+// })();
